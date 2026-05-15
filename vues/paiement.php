851a36316ae5fc1967a528696ec6d4470d1b95e1
leMecanicien-beep <anesmokrane05@gmail.com>
2026-05-15 11:40:35 +0200

    Update paiement.php

--- vues/paiement.php
+++ vues/paiement.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require_once __DIR__ . '/../lib/users.php';
+verifier_session_revoquee();
 
 if (!isset($_SESSION['user'])) {
     header('Location: connexion.php');
@@ -18,14 +19,18 @@ $type  = $_POST['type'];
 $panier = $_POST['panier'];
 $heure_souhaitee = !empty($_POST['heure_souhaitee']) ? $_POST['heure_souhaitee'] : '';
 
+// Générer un identifiant de transaction unique
 $transaction = strtoupper(substr(md5(uniqid()), 0, 12));
 
+// Vendeur
 $vendeur = 'SUPMECA_A';
 
+// URL de retour
 $retour = 'http://127.0.0.1:8000/vues/retour_paiement.php?type=' . $type
         . '&panier=' . urlencode($panier)
         . '&heure=' . urlencode($heure_souhaitee);
 
+// Calcul du control
 require_once __DIR__ . '/../lib/getapikey.php';
 $api_key = getAPIKey($vendeur);
 $control = md5($api_key . '#' . $transaction . '#' . $total . '#' . $vendeur . '#' . $retour . '#');
